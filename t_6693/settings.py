d7a6ed7ddc6ac5db62422752d963125792c10adb
Evgeny <info@tinamagic.lv>
2025-01-06 15:24:54 +0200

    Start

--- t_6693/settings.py
+++ t_6693/settings.py
@@ -99,8 +99,8 @@ WSGI_APPLICATION = 't_6693.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
