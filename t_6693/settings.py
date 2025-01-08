5e719dc0d7cf53e1013bfa5692fadb4c9e0a1238
Evgeny <info@tinamagic.lv>
2025-01-08 16:59:32 +0200

    Start

--- t_6693/settings.py
+++ t_6693/settings.py
@@ -105,9 +105,9 @@ DATABASES = {
     }
 }
 
-if os.getenv('DATABASE_URL'):
-    db_from_env = dj_database_url.config(conn_max_age=600, conn_health_checks=True,)
-    DATABASES['default'].update(db_from_env)
+# if os.getenv('DATABASE_URL'):
+#     db_from_env = dj_database_url.config(conn_max_age=600, conn_health_checks=True,)
+#     DATABASES['default'].update(db_from_env)
 
 
 # Password validation
