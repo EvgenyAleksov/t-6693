24f14b8414d36916e41fd91f01a73653f9fc8a05
Evgeny <info@tinamagic.lv>
2025-01-11 15:06:43 +0200

    Start

--- t_6693/calc/forms.py
+++ t_6693/calc/forms.py
@@ -1,27 +1,27 @@
+from datetime import datetime  # type: ignore
+
 from django import forms  # type: ignore
-
-
-class MyDateInput(forms.DateInput):
-    input_type = "date"
-    # format = "%d-%m-%Y"
 
 
 class CalcForm_1(forms.Form):
     name = forms.CharField(
-        max_length=25,
-        required=False,
-        label="Имя",
-        help_text="(по желанию)"
+        max_length=25, required=False, label="Имя", help_text="(по желанию)"
     )
+
     birthday = forms.DateField(
-        # widget=MyDateInput(attrs={"class": "form-control", "type": "date"}),
-        widget=MyDateInput({"class": "form-control"}),
+        input_formats=["%d.%m.%Y"],
+        widget=forms.DateInput(
+            attrs={
+                type: "date",
+                "data-mask": "00.00.0000",
+            }
+        ),
         required=True,
         label="Дата рождения",
     )
 
     def clean_birthday(self):
-        return val_bd(self.cleaned_data.get("birthday"))    
+        return val_bd(self.cleaned_data.get("birthday"))
 
 
 class CalcForm_2(forms.Form):
@@ -32,8 +32,14 @@ class CalcForm_2(forms.Form):
         help_text="(по желанию)",
     )
     birthday_1 = forms.DateField(
+        input_formats=["%d.%m.%Y"],
+        widget=forms.DateInput(
+            attrs={
+                type: "date",
+                "data-mask": "00.00.0000",
+            }
+        ),
         required=True,
-        widget=MyDateInput({"class": "form-control"}),
         label="Дата рождения 1",
     )
     name_2 = forms.CharField(
@@ -43,21 +49,28 @@ class CalcForm_2(forms.Form):
         help_text="(по желанию)",
     )
     birthday_2 = forms.DateField(
+        input_formats=["%d.%m.%Y"],
+        widget=forms.DateInput(
+            attrs={
+                type: "date",
+                "data-mask": "00.00.0000",
+            }
+        ),
         required=True,
-        widget=MyDateInput({"class": "form-control"}),
         label="Дата рождения 2",
     )
 
     def clean_birthday_1(self):
-        return val_bd(self.cleaned_data.get("birthday_1"))    
+        return val_bd(self.cleaned_data.get("birthday_1"))
 
     def clean_birthday_2(self):
-        return val_bd(self.cleaned_data.get("birthday_2"))    
+        return val_bd(self.cleaned_data.get("birthday_2"))
 
 
 def val_bd(bd):
-    if bd.year not in range(1900, 2101):
-        raise forms.ValidationError(
-            "Допустимое значение года от 1900 до 2100"
-        )
+    bd1 = bd.strftime("%d.%m.%Y")
+    year = datetime.strptime(bd1, "%d.%m.%Y").year
+    if year not in range(1900, 2101):
+        raise forms.ValidationError("Допустимое значение года от 1900 до 2100")
     return bd
+
