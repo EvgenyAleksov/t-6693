1db6b579383ae687443af7cd9882c17509b40e76
Evgeny <info@tinamagic.lv>
2025-01-12 17:57:53 +0200

    Start

--- t_6693/users/admin.py
+++ t_6693/users/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin  # type: ignore
 from django.contrib.auth.admin import UserAdmin  # type: ignore
 
-# from t_6693.users.forms import UserForm, UserUpdateForm  # type: ignore
+from t_6693.users.forms import UserForm, UserUpdateForm  # type: ignore
 from t_6693.users.models import User  # type: ignore
 
-# class UserAdmin(UserAdmin):
-#     add_form = UserForm
-#     form = UserUpdateForm
-#     model = User
-#     list_display = ['username', 'email']
+
+class UserAdmin(UserAdmin):
+    add_form = UserForm
+    form = UserUpdateForm
+    model = User
+    list_display = ['username', 'email']
+
 
 admin.site.register(User, UserAdmin)
